8617ecab6b8a05f504e4e13e03bbf2cbeee680fd
Laxmi Prasanna <laxmiprasanna.artham@radiant.digital>
2024-10-19 00:56:46 +0530

    first commit

---
+++ src/app/components/header/Header.js
@@ -0,0 +1,10 @@
+import styles from './header.module.css';
+
+
+function Header(){
+    return <div className={styles.container}>
+        <h1>My Portfolio</h1>
+    </div>
+}
+
+export default Header;
